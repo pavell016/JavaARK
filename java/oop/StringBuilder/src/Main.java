bc8562060bba388e02f7bcd0fcc8ccb1d18f8fc5
pavell <pavellupan29@gmail.com>
2025-02-07 10:36:04 +0100

    ex5

--- java/oop/StringBuilder/src/Main.java
+++ java/oop/StringBuilder/src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+Los objetivos de este ejercicio son:
+     Trabajar con la clase StringBuilder
+     Entender las diferencias entre String y StrinBuilder
+La clase StringBuilder permite que trabajemos con cadenas de texto de forma más eficiente en
+nuestras aplicaciones cuando tenemos que modificarlas de forma frecuente. A continuación puedes
+ver el código de una aplicación donde a partir de una string “Dot saw I was Tod” se pretende
+mostrarla de forma invertida “doT saw I was toD”. El código utiliza la clase String. ¿Puedes
+mejorarlo usando StringBuilder?
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        char[] tempCharArray = new char[len];
+        char[] charArray = new char[len];
+// put original string in an
+// array of chars
+        for (int i = 0; i < len; i++) {
+            tempCharArray[i] =
+                    palindrome.charAt(i);
         }
+// reverse array of chars
+        for (int j = 0; j < len; j++) {
+            charArray[j] =
+                    tempCharArray[len - 1 - j];
+        }
+        String reversePalindrome =
+                new String(charArray);
+        System.out.println(reversePalindrome);
+
+        //stringBuilder HAHA
+        StringBuilder pali = new StringBuilder("Dot saw I was Tod");
+        System.out.println(pali.reverse().toString());
     }
 }
